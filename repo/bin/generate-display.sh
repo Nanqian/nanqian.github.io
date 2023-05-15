ddc3d5e48de51d454779393be7571783c0a25fff
nanqian <497899268@qq.com>
2023-05-16 00:03:49 +0800

    add jailbreak repo

---
+++ repo/bin/generate-display.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+json_object=$(cat << EOF
+{
+    "contact": {
+        "twitter": "GinsuDev",
+        "discord": "https://discord.gg/BhdUyCbgkZ"
+    },
+    "information": {
+        "description": "your_description",
+        "source_code_link": ""
+    },
+    "changelog": [
+    {
+        "date": "$(date +%Y-%m-%d)",
+        "version_number": "1.0.0",
+        "changes": "- Initial release"
+    }
+    ]
+}
+EOF
+)
+
+echo $json_object
+
